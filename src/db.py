4ec3ab80524afbe7d3b196f63e2fe4fd31be6eb8
Samriti Sadhu <ssadhu@scottlogic.com>
2025-02-11 13:50:15 +0000

    parametrised queries

--- src/db.py
+++ src/db.py
@@ -56,19 +56,22 @@ def search_issues(params: IssuesParams):
 
 def search_provision_summary(params: ProvisionParams):
     s3_uri = f"s3://{config.collection_bucket}/{config.performance_base_path}/*.parquet"  # noqa
-    pagination = f"LIMIT {params.limit} OFFSET {params.offset}"
 
     where_clause = ""
+    query_params = []
+
     if params.dataset:
-        where_clause += _add_condition(where_clause, f"dataset = '{params.dataset}'")
+        where_clause += _add_condition(where_clause, "dataset = ?")
+        query_params.append(params.dataset)
+
     if params.organisation:
-        where_clause += _add_condition(
-            where_clause, f"organisation = '{params.organisation}'"
-        )
+        where_clause += _add_condition(where_clause, "organisation = ?")
+        query_params.append(params.organisation)
 
     sql_count = f"SELECT COUNT(*) FROM '{s3_uri}' {where_clause}"
+    sql_results = f"SELECT * FROM '{s3_uri}' {where_clause} LIMIT ? OFFSET ?"
+
     logger.debug(sql_count)
-    sql_results = f"SELECT * FROM '{s3_uri}' {where_clause} {pagination}"
     logger.debug(sql_results)
 
     with duckdb.connect() as conn:
@@ -80,44 +83,56 @@ def search_provision_summary(params: ProvisionParams):
                     ).fetchall()
                 )
                 logger.debug(conn.execute("FROM duckdb_secrets();").fetchall())
-            count = conn.execute(sql_count).fetchone()[
-                0
-            ]  # Count is first item in Tuple
-            results = conn.execute(sql_results).arrow().to_pylist()
+
+            # Execute parameterized queries
+            count = conn.execute(sql_count, query_params).fetchone()[0]
+            results = (
+                conn.execute(sql_results, query_params + [params.limit, params.offset])
+                .arrow()
+                .to_pylist()
+            )
+
             return PaginatedResult(
                 params=PaginationParams(offset=params.offset, limit=params.limit),
                 total_results_available=count,
                 data=results,
             )
         except Exception as e:
-            logger.exception(
-                "Failure executing DuckDB queries",
-            )
+            logger.exception("Failure executing DuckDB queries")
             raise e
 
 
 def get_specification(params: SpecificationsParams):
     s3_uri = f"s3://{config.collection_bucket}/{config.specification_base_path}/*.parquet"  # noqa
-    pagination = f"LIMIT {params.limit} OFFSET {params.offset}"
 
     where_clause = ""
+    query_params = {}
 
     if params.dataset:
         where_clause += _add_condition(
             where_clause,
-            f"TRIM(BOTH '\"' FROM json_extract(json(value), '$.dataset')) = '{params.dataset}'",
+            "TRIM(BOTH '\"' FROM json_extract(json(value), '$.dataset')) = ?",
         )
+        query_params["dataset"] = params.dataset
 
     sql_count = f"""
-    SELECT COUNT(*) FROM (SELECT unnest(CAST(json AS VARCHAR[])) AS value 
-    FROM '{s3_uri}') as parsed_json {where_clause} {pagination}
+    SELECT COUNT(*) FROM (
+        SELECT unnest(CAST(json AS VARCHAR[])) AS value 
+        FROM '{s3_uri}'
+    ) AS parsed_json {where_clause}
+    LIMIT ? OFFSET ?
     """
-    logger.debug(sql_count)
+
     sql_results = f"""
-    SELECT value as json FROM 
-    (SELECT unnest(CAST(json AS VARCHAR[])) AS value FROM '{s3_uri}') AS parsed_json
-    {where_clause} {pagination}
+    SELECT value AS json FROM (
+        SELECT unnest(CAST(json AS VARCHAR[])) AS value 
+        FROM '{s3_uri}'
+    ) AS parsed_json
+    {where_clause} 
+    LIMIT ? OFFSET ?
     """
+
+    logger.debug(sql_count)
     logger.debug(sql_results)
 
     with duckdb.connect() as conn:
@@ -130,12 +145,19 @@ def get_specification(params: SpecificationsParams):
                 )
                 logger.debug(conn.execute("FROM duckdb_secrets();").fetchall())
 
-            count = conn.execute(sql_count).fetchone()[
-                0
-            ]  # Count is first item in Tuple
-            results = conn.execute(sql_results).arrow().to_pylist()
+            # Execute queries with parameters
+            count = conn.execute(
+                sql_count, [*query_params.values(), params.limit, params.offset]
+            ).fetchone()[0]
+            results = (
+                conn.execute(
+                    sql_results, [*query_params.values(), params.limit, params.offset]
+                )
+                .arrow()
+                .to_pylist()
+            )
 
-            # Extract and parse the JSON field
+            # Convert JSON strings to actual JSON objects
             json_results = []
             for item in results:
                 if "json" in item and isinstance(item["json"], str):
@@ -151,9 +173,7 @@ def get_specification(params: SpecificationsParams):
                 data=json_results,
             )
         except Exception as e:
-            logger.exception(
-                "Failure executing DuckDB queries",
-            )
+            logger.exception("Failure executing DuckDB queries")
             raise e
 
 

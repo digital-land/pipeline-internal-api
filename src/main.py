368c5ec4c72f9b00783d79394c291cbf605d1b7b
Matt Poole <matt.a.poole@outlook.com>
2026-05-20 10:13:16 +0100

    lint

--- src/main.py
+++ src/main.py
@@ -116,7 +116,9 @@ def dataset_resource_mapping(
 
 
 @app.get("/performance/endpoint_dataset_summary", tags=["endpoint_dataset_summary"])
-def endpoint_dataset_summary(http_response: Response, params: EndpointDatasetSummaryParams = Depends()):
+def endpoint_dataset_summary(
+    http_response: Response, params: EndpointDatasetSummaryParams = Depends()
+):
     paginated_result = db.search_endpoint_dataset_summary(params)
     http_response.headers["X-Pagination-Total-Results"] = str(
         paginated_result.total_results_available
